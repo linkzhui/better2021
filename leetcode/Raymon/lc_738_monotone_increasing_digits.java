3f6a3444dc025dc2a0f76b7f471eb4e0d5f9bfdf
zhemin <zhemin@zhemins-MacBook-Pro.local>
2021-06-04 22:46:59 -0700

    周五放松一下，就刷一道题目，明天起来继续努力

---
+++ leetcode/Raymon/lc_738_monotone_increasing_digits.java
@@ -0,0 +1,45 @@
+package Raymon;
+
+public class lc_738_monotone_increasing_digits {
+    int num[] = new int[10];
+
+    //32 -> 29
+    //332 -> 299
+    //232 -> 299
+    //132 - > 99
+
+    //我们发现一个规律，从右往左走，每次发现当前的数字不满足monotone increasing digits，我们就把current index value - 1, 然后current index往右的数字全部变成9，就可以产生一组新的数字，是当前index的最大monotone increasing digits
+    public int monotoneIncreasingDigits(int n) {
+        if (n < 10) {
+            return n;
+        }
+        int count = 0;
+        while (n > 0) {
+            int cur_num = n % 10;
+            n = n / 10;
+            if (count == 0) {
+                num[count] = cur_num;
+            } else {
+                if (cur_num <= num[count-1]) {
+                    num[count] = cur_num;
+                } else {
+                    helper(count, cur_num);
+                }
+            }
+            count++;
+        }
+        int result = 0;
+        for (int i = 0; i < count; i++) {
+            result += num[i] * Math.pow(10, i);
+        }
+        return result;
+
+    }
+
+    private void helper(int index, int cur_num) {
+        num[index] = cur_num - 1;
+        for (int i = index - 1; i >= 0; i--) {
+            num[i] = 9;
+        }
+    }
+}
